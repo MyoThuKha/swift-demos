d820d3fe405c4d02131e729fede1283e6c0367e4
Myo Thu Kha <myothukha.dev@gmail.com>
2024-07-21 11:31:31 +0700

    Initial Commit

---
+++ swift-demos/swift_demosApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_demosApp.swift
+//  swift-demos
+//
+//  Created by Myo Thu Kha on 7/21/24.
+//
+
+import SwiftUI
+
+@main
+struct swift_demosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
